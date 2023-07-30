4a87a07fe47e6dbbc3e8cfae4c95bfa2562154ee
Meeran Tajalli <131452235+MeeranTajalli@users.noreply.github.com>
2023-07-30 22:06:09 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <div>
+      <img
+        className="pic"
+        alt="wraith's kunai"
+        src="https://dotesports.com/wp-content/uploads/2023/01/18181636/1153033.jpg"
+        img
+      />
+      <img
+        alt="looking goood"
+        src="https://s1.zerochan.net/Wraith.%28Apex.Legends%29.600.3878371.jpg"
+        img
+      />
+      <img
+        className="pic"
+        alt="In game"
+        src="https://cdn.vox-cdn.com/thumbor/uIIJHwjzSyo2uqDrH_8pNBnHtKs=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21938762/wraith.jpg"
+        img
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
